f5be4197f0b9f039142993688a1a3b1adb61b0c3
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-04-29 16:01:18 -0300

    first commit

---
+++ laravel1/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/teste','teste');
+Route::view('/login','login');
+
+Route::get('/noticia/{slug}', function($slug){
+    echo "Slug: ".$slug;
+});
+Route::get('/noticia/{slug}/comentario/{id}', function($slug, $id){
+    echo "Mostrando o comentário".$id." da notícia ".$slug;
+});
+
+Route::get('/user/{name}', function($name){
+    echo "Mostrando usuário de NOME: ".$name;
+})->where('name', '[a-z]+');
+
+
+Route::get('/user/{id}', function($id){
+    echo "Mostrando usuário de ID: ".$id;
+});
